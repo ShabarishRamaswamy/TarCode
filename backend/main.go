14b02f3045dbb46c8a8615f004f5bbaa9d13bc74
Shabarish Ramaswamy <shabarishramaswamy@gmail.com>
2025-09-23 15:42:25 -0700

    Added crude WORKING examples of running C and Python code

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os/exec"
 )
 
 func main() {
@@ -14,10 +16,60 @@ func main() {
 	server := http.NewServeMux()
 	// Handle all requests by serving a file from the "build" directory
 	server.Handle("/", fs)
-	server.HandleFunc("/api/submission/", func(w http.ResponseWriter, r *http.Request) {
+
+	// There will only be 1 user [For Now]
+	// Body: { "code": "code", "test_cases": [case1, case2, ...] }
+	server.HandleFunc("/api/submission/{id}", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
-			body, _ := io.ReadAll(r.Body)
-			fmt.Println(body)
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "Can't read body", http.StatusBadRequest)
+				return
+			}
+
+			idString := r.PathValue("id")
+			fmt.Printf("ID: %v", idString)
+
+			fmt.Println(string(body))
+
+			// Save the file
+			// err = os.WriteFile(fmt.Sprintf("%s/%s", "./submissions", idString), body, 0644)
+			// check(err)
+
+			// Compile & Run (PYTHON example)
+			out, err := exec.Command("python3", "./submissions/123.py").Output()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Output: %s\n", out)
+
+			// Compile and Run (C example)
+			// Compile
+			cmd := exec.Command("gcc", "./submissions/123.c", "-o", "./submissions/123.out")
+			err = cmd.Run()
+			log.Printf("Command finished with error: %v", err)
+
+			// Run this code
+			cmd = exec.Command("./submissions/123.out")
+			stdout, err := cmd.StdoutPipe()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if err := cmd.Start(); err != nil {
+				log.Fatal(err)
+			}
+
+			// Get the Results back
+			buf := new(bytes.Buffer)
+			buf.ReadFrom(stdout)
+
+			fmt.Println("Outout: ", buf)
+
+			// Compare with the given test cases
+
+			fmt.Fprintf(w, "Submission received!")
+			return
 		}
 	})
 
